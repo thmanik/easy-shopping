80b10245a7d6f31516568128fb910aeb59219045
manikOnline <thmanik1241@gmail.com>
2024-02-20 18:11:39 +0600

    components

---
+++ src/components/HeaderTop.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderTop = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HeaderTop;
---
+++ src/components/MobNavbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MobNavbar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MobNavbar;
